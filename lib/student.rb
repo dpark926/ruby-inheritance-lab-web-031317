c1af3b6e51bbe90a21bcae90d2f203266aed9c1a
Daniel Park <dpeace926@gmail.com>
2017-03-26 19:45:38 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(teach)
+    @knowledge << teach
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
+
+KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
